b9ce8af774b24ae1a6a31743a4790e18fc3bef65
AndrewUkraine <andrewukraine17@gmail.com>
2018-07-14 15:05:42 +0300

    it works. but only searches one values for one page

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -35,68 +35,66 @@ public class Parser  {
             e.printStackTrace();
         }
 
-        Elements elements = null;
-        if (doc != null) {
+        Elements elements;
+
            // elements = doc.select("html body main#app section.styles__layout--1QK8W.styles__stretchLayout--3omnx div div.styles__container--2cj5w div.row div.col-sm-9.col-md-10 div div div.styles__container--1bqmB div.row div.styles__container--1bqmB div.styles__tile--2s8XN.col-sm-6.col-md-4.col-lg-4 div a");
-
-            elements = doc.select("a.href");
-
-           /* if (URL.indexOf("/p/") == 0) {
-                URL.add("https://www.aboutyou.de" + url);*/
+            elements =  doc.body().getElementsByClass("styles__tile--2s8XN").first().getElementsByAttribute("href");
 
 
-
-        }
-        if (elements != null) {
             for (Element element : elements) {
-                String url3 = "https://www.aboutyou.de" + element.attr("href");
+                String url = element.attr("href");
 
-                Document doc3 = null;
-                try {
-                    doc3 = Jsoup.connect(url3).get();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Elements elements1 = doc3.select(".productPrices"); //price
-                Elements elements2 = doc3.select(".priceStyles__strike--PSBGK"); //initialPrice
-                Elements elements3 = doc3.select("h1"); //brand
-                Elements elements4 = doc3.select(".styles__title--UFKYd"); //color
-                Elements elements5 = doc3.select(".styles__title--3Jos_"); // name
-                Elements elements6 = doc3.select(".styles__articleNumber--1UszN"); //articul
-                Elements elements7 = doc3.select(".styles__textElement--3QlT_"); //description
-                Elements elements8 = doc3.select(".styles__label--1cfc7"); //shippingCosts
+                //if (url.indexOf("/p/") != 0) {
 
-                httpRequests++;
+                    String url3 = "https://www.aboutyou.de" + url;
 
-                List<Offer> offers = new ArrayList<>();
-
-                Offer offer = new Offer();
-
-                offer.setName(cleanHTML(elements5.text()));
-                offer.setBrand(cleanHTML(brandCleaner(elements3.text())));
-                offer.setColor(cleanHTML(elements4.text()));
-                offer.setPrice(cleanHTML(elements1.text()));
-                offer.setInitialPrice(cleanHTML(elements2.text()));
-                offer.setDescription(cleanHTML(elements7.text()));
-                offer.setArticleId(cleanHTML(elements6.text()));
-                offer.setShippingCosts(cleanHTML(elements8.text()));
-
-                offers.add(offer);
-                offers.forEach(System.out::println);
-
-                XMLView xmlView = new XMLView();
-                xmlView.update(offers);
-
-                if (httpRequests % 5 == 0) {
-                    System.out.println("Sleeping for 2 sec");
+                    Document doc3 = null;
                     try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
+                        doc3 = Jsoup.connect(url3).get();
+                    } catch (IOException e) {
                         e.printStackTrace();
+                    }
+                    Elements elements1 = doc3.select(".productPrices"); //price
+                    Elements elements2 = doc3.select(".priceStyles__strike--PSBGK"); //initialPrice
+                    Elements elements3 = doc3.select("h1"); //brand
+                    Elements elements4 = doc3.select(".styles__title--UFKYd"); //color
+                    Elements elements5 = doc3.select(".styles__title--3Jos_"); // name
+                    Elements elements6 = doc3.select(".styles__articleNumber--1UszN"); //articul
+                    Elements elements7 = doc3.select(".styles__textElement--3QlT_"); //description
+                    Elements elements8 = doc3.select(".styles__label--1cfc7"); //shippingCosts
+
+                    httpRequests++;
+
+                    List<Offer> offers = new ArrayList<>();
+
+                    Offer offer = new Offer();
+
+                    offer.setName(cleanHTML(elements5.text()));
+                    offer.setBrand(cleanHTML(brandCleaner(elements3.text())));
+                    offer.setColor(cleanHTML(elements4.text()));
+                    offer.setPrice(cleanHTML(elements1.text()));
+                    offer.setInitialPrice(cleanHTML(elements2.text()));
+                    offer.setDescription(cleanHTML(elements7.text()));
+                    offer.setArticleId(cleanHTML(elements6.text()));
+                    offer.setShippingCosts(cleanHTML(elements8.text()));
+
+                    offers.add(offer);
+                    offers.forEach(System.out::println);
+
+                    XMLView xmlView = new XMLView();
+                    xmlView.update(offers);
+
+                    if (httpRequests % 5 == 0) {
+                        System.out.println("Sleeping for 2 sec");
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
         }
-        }
+
         long runTime = (System.nanoTime() - startTime) / 10000000;
         System.out.println("Run-time = " + runTime / 100 + " sec");
         System.out.println("Amount of triggered HTTP request " + httpRequests);
@@ -106,7 +104,7 @@ public class Parser  {
 
 
 }
-  }
+
 
     private static String cleanHTML(String html) {
         return html
