1bcae8dccf51c5aed8e72ddd051a25c2546270df
AndrewUkraine <andrewukraine17@gmail.com>
2018-07-20 22:07:48 +0300

    added BD 2

--- src/main/resources/initDB.sql
+++ src/main/resources/initDB.sql
@@ -6,13 +6,13 @@ CREATE SEQUENCE global_seq START 100000;
 CREATE TABLE offer
 (
   id            INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-  name          VARCHAR(200)        DEFAULT 'no result',
-  brand         VARCHAR(100)        DEFAULT 'no result',
-  color         VARCHAR(100)        DEFAULT 'no result',
+  name          VARCHAR(200)        DEFAULT NULL ,
+  brand         VARCHAR(100)        DEFAULT NULL ,
+  color         VARCHAR(100)        DEFAULT NULL ,
   price         VARCHAR(100)        DEFAULT NULL,
-  initialPrice  VARCHAR(100)        DEFAULT NULL,
-  description   VARCHAR(500)        DEFAULT 'no result',
-  articleId     VARCHAR(100)        DEFAULT NULL,
+  initialPrice  VARCHAR(100)        DEFAULT NULL ,
+  description   VARCHAR(500)        DEFAULT NULL ,
+  articleId     VARCHAR(100)        DEFAULT NULL ,
   shippingCosts VARCHAR(100)        DEFAULT NULL
 );
 
